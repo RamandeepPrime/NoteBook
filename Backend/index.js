261089b484c51eb4cd7ac169641b882d8d80a191
RamandeepPrime <ramandeep9540479976@gmail.com>
2021-12-24 12:14:28 +0530

    creating routes and models for app and redirecting it from index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,15 @@ connectToMongo();
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// we uses routes in routes folder by app.use
+app.use('/api/auth',require("./routes/Auth"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
