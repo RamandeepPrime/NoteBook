55a9eef316436680908f5004f59ef4bd04d6ba47
RamandeepPrime <ramandeep9540479976@gmail.com>
2021-12-25 15:21:42 +0530

    Give User token when they finally login, Securing users password with bycrypt and added to database

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
-const name=require("./config.json");
+const { name,password }=require("./config.json");
 const dbname="iNoteBook";
-const mongoURI=`mongodb+srv://${name.name}:${name.password}@ramandeepatlas.w8fqr.mongodb.net/${dbname}?authSource=admin&replicaSet=atlas-13ov13-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+const mongoURI=`mongodb+srv://${name}:${password}@ramandeepatlas.w8fqr.mongodb.net/${dbname}?authSource=admin&replicaSet=atlas-13ov13-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
 const connectToMongo=()=>{
 	mongoose.connect(mongoURI,()=>{
 		console.log("connected to Mongo succesfully");
--- Backend/routes/Auth.js
+++ Backend/routes/Auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const User = require("../models/User");
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secretjwtkey=require("../config.json").jwtToken;
+
+
 const router = express.Router();
 
 //we cannot use req.body unless we pass the middle ware 
@@ -32,15 +37,31 @@ router.post('/createuser',
 				// if exist then return this
 				return res.status(404).json({ error: "Please enter valid credentials" });
 			}
+			
+			// creating secured password that can be stored in database
+
+			const salt = bcrypt.genSaltSync(10);
+			const secPassword  = bcrypt.hashSync(req.body.password, salt);
+
+
 			user = await User.create({
 				name: req.body.name,
 				email: req.body.email,
-				password: req.body.password,
+				password: secPassword,
 			})
-			res.json(user);
+
+			jwtData={
+				user:{
+					id:user.id
+				}
+			}
+
+			const jwtAuthToken = jwt.sign(jwtData, secretjwtkey);
+
+			res.json({authToken:jwtAuthToken});
 
 		} catch (error) {
-			
+
 			console.error(error.message);
 			res.status(500).send("Error occured in auth");
 		}
